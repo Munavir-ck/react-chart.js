8886cad1d2abc5a896ef1a32b452909fc540fe76
Munavir <munavirokv@gmail.com>
2023-11-23 07:58:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Dashboard from './Pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Dashboard/>
+      
+    </div>
+  );
+}
+
+export default App;

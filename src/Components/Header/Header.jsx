a7bfb7bc8c05d78087a2164c9104d1fcf989755b
Munavir <munavirokv@gmail.com>
2023-11-23 09:09:42 +0530

    edited

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import { GiRoundShield } from "react-icons/gi";
 import { FaArrowDown } from "react-icons/fa";
+import icon from "../../Assets/image 3.png";
 
 const Header = ({isActive, setActive}) => {
   return (
     <div className="fixed top-0 left-0 right-0 flex flex-col sm:flex-row w-full  bg-gray-500 h-28">
    
-      <div className="sm:h-full sm:w-56   bg-gray-500 flex items-center justify-center h-16 shadow-lg">
+      <div className="sm:h-full sm:w-56   bg-gray-500 flex items-center justify-center h-16 sm:shadow-lg">
         <img
-          src=""
+          src={icon}
           className="h-full w-full object-contain"
           alt="Header Icon"
         />
